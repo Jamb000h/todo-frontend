b7093f7b9a8ab3df0a7b9d06282a1560ed4139e2
JereHolopainen <jerzqaah@gmail.com>
2020-01-03 19:42:58 +0200

    Add useState and display new Todos

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 import TodoList from './TodoList';
 import TodoForm from "./TodoForm";
 
 function App() {
 
-  const todos = ["todo1", "todo2", "todo3"];
+  /* const todos = ["todo1", "todo2", "todo3"]; */
+  const [todos, setTodo] = useState(["todo1", "todo2", "todo3"]);
 
   return (
     <div className="App">
-      <TodoForm />
+      <TodoForm saveTodo={ todoText => {
+          const trimmedText = todoText.trim();
+
+          if(trimmedText.length > 0){
+            setTodo([...todos, trimmedText]);
+          }
+        }} 
+
+      />
       <TodoList todos={todos} />
     </div>
   );
--- src/TodoForm.js
+++ src/TodoForm.js
@@ -1,14 +1,26 @@
-import React from "react";
+import React, {useState} from "react";
 
 const TodoForm = props => {
+    
+    const [value, setValue] = useState("");
 
     return (
-        <form>
+        <form
+            onSubmit={ event => {
+                event.preventDefault();
+                props.saveTodo(value);
+                console.log(value);
+            }}
+        >
             <label>
                 Todo:
                 <input 
                     type="text"
                     placeholder="Todo Text..."
+                    onChange={event => {
+                        setValue(event.target.value);
+                    }}
+                    value={value}
                 />
             </label>
             <input type="submit" value="Add Todo" />
